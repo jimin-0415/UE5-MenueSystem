0ab3b7f5c9a26966112c28a95e7e30aaff05f71d
jiminKim <programmer0415@gmail.com>
2023-08-05 18:21:21 +0900

    Feat: Accessing Subsystem
    - Button callbacks
    - Onscreen DebugMessages
    - Accessed the MultiPlayer Sessions Subsystem

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Menu.h"
+#include "MultiPlayerSessionsSubsystem.h"
+#include "Components/Button.h"
 
 
 ////////////////////////////////////////////////////////////////////////////
@@ -28,4 +30,69 @@ void UMenu::MenuSetup()
 		}
 	}
 
+	// GameInstace 를 통해서 SubSystem 정보를 가져옵니다.
+	UGameInstance* gameInstance = GetGameInstance();
+	if ( gameInstance )
+	{
+		m_MultiPlayerSessionSubsystem = gameInstance->GetSubsystem< UMultiPlayerSessionsSubsystem >();
+	}
+
+}
+
+////////////////////////////////////////////////////////////////////////////
+/// 초기화를 진행합니다.
+////////////////////////////////////////////////////////////////////////////
+bool UMenu::Initialize()
+{
+	if ( !Super::Initialize() )
+		return false;
+
+	if ( m_HostButton )
+	{
+		m_HostButton->OnClicked.AddDynamic( this, &ThisClass::HostButtonClicked );
+	}
+
+	if ( m_JoinButton )
+	{
+		m_JoinButton->OnClicked.AddDynamic( this, &ThisClass::JoinButtonClicked );
+	}
+
+
+	return true;
+}
+
+////////////////////////////////////////////////////////////////////////////
+/// Host 버튼을 클릭합니다.
+////////////////////////////////////////////////////////////////////////////
+void UMenu::HostButtonClicked()
+{
+	if ( GEngine )
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			15.f,
+			FColor::Yellow,
+			FString( TEXT( "Host Button Clicked" ) ) );
+	}
+
+	if ( m_MultiPlayerSessionSubsystem )
+	{
+		/// TODO. 일단 들어오는지 검사하기 위해서 임시로 적용.
+		m_MultiPlayerSessionSubsystem->CreateSession( 4, FString( "FreForAll" ) );
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////
+/// Join 버튼을 클릭합니다..
+////////////////////////////////////////////////////////////////////////////
+void UMenu::JoinButtonClicked()
+{
+	if ( GEngine )
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			15.f,
+			FColor::Yellow,
+			FString( TEXT( "Join Button Clicked" ) ) );
+	}
 }
--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/Menu.h
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/Menu.h
@@ -6,6 +6,10 @@
 #include "Blueprint/UserWidget.h"
 #include "Menu.generated.h"
 
+
+class UButton;
+class UMultiPlayerSessionsSubsystem;
+
 /**
  * 
  */
@@ -17,4 +21,33 @@ public:
 	/// 메뉴 초기 설정을 합니다.
 	UFUNCTION( BlueprintCallable )
 	void MenuSetup();
+
+protected:
+	/// 초기화를 진행합니다.
+	virtual bool Initialize() override;
+
+private:
+	/// 바인딩되는 버튼의 이름은 블루프린트에 정의된 이름과 동일해야 한다.
+	// UPROPERTY( meta = ( BindWidget ) ) 
+
+	/// Host 버튼
+	UPROPERTY( meta = ( BindWidget ) )
+	UButton* m_HostButton;
+
+	/// Join 버튼
+	UPROPERTY( meta = ( BindWidget ) )
+	UButton* m_JoinButton;
+
+private:
+	/// Host 버튼을 클릭합니다.
+	UFUNCTION()
+	void HostButtonClicked();
+
+	/// Join 버튼을 클릭합니다.
+	UFUNCTION()
+	void JoinButtonClicked();
+
+private:
+	/// The subsystem designed to handl all online session functionality
+	UMultiPlayerSessionsSubsystem* m_MultiPlayerSessionSubsystem;
 };
